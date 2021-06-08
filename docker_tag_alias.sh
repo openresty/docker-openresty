5fcb5b64d943fdb68a51934b47d5687d88ef6cdb
Evan Wies <evan@neomantra.net>
2021-06-08 17:56:27 -0400

    Fix amzn2-amd64 and pull first in docker_tag_alias.

--- docker_tag_alias.sh
+++ docker_tag_alias.sh
@@ -15,6 +15,7 @@ ALIAS="$2"
 cat /tmp/docker.pass | docker login -u="$DOCKER_USERNAME" --password-stdin
 
 if [[ "$TRAVIS_BRANCH" == "master" ]] ; then
+    docker pull $DOCKER_ORG/openresty:$FLAVOR &&
     docker tag $DOCKER_ORG/openresty:$FLAVOR $DOCKER_ORG/openresty:$ALIAS &&
     docker push $DOCKER_ORG/openresty:$ALIAS ;
 fi
@@ -22,9 +23,11 @@ fi
 if [[ "$TRAVIS_TAG" ]] ; then
     TRAVIS_TAG_BASE=$(echo -n "$TRAVIS_TAG" | sed 's/-[0-9]$//g') ;
     if [[ ( "$TRAVIS_TAG_BASE" ) && ( "$TRAVIS_TAG_BASE" != "$TRAVIS_TAG" ) ]] ; then
-    docker tag $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$FLAVOR $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$ALIAS &&
-    docker push $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$ALIAS ;
+        docker pull $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$FLAVOR &&
+        docker tag $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$FLAVOR $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$ALIAS &&
+        docker push $DOCKER_ORG/openresty:$TRAVIS_TAG_BASE-$ALIAS ;
     fi ;
+    docker pull $DOCKER_ORG/openresty:$TRAVIS_TAG-$FLAVOR &&
     docker tag $DOCKER_ORG/openresty:$TRAVIS_TAG-$FLAVOR $DOCKER_ORG/openresty:$TRAVIS_TAG-$ALIAS &&
     docker push $DOCKER_ORG/openresty:$TRAVIS_TAG-$ALIAS ;
 fi
